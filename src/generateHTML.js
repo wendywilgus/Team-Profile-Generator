19a86c200f88a4e46c03058cc5a95c1f64413e05
Wendy Wilgus <wilguswendy@gmail.com>
2022-10-15 07:20:24 -0400

    added to generatehtml

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -19,4 +19,35 @@ const generateHTML = Employee =>  {
         .filter(Employee => Employee.getTitle() === "Intern")
         .map(Manager => writeIntern(intern))
     );
+    return newFile(writeHTML.join(""));
+}
+
+const writeManager = manager => {
+    let template = fs.readFileSync(path.resolve(cards, "manager.html"), "utf8");
+    template = replacePlaceholders(template, "name", manager.getName());
+    template = replacePlaceholders(template, "title", manager.getTitle());
+    template = replacePlaceholders(template, "email", manager.getEmail());
+    template = replacePlaceholders(template, "id", manager.getID());
+    template = replacePlaceholders(template, "officeNumber", manager.getOfficeNumber());
+    return template;
+}
+
+const writeEngineer = engineer => {
+    let template = fs.readFileSync(path.resolve(cards, "engineer.html"), "utf8");
+    template = replacePlaceholders(template, "name", engineer.getName());
+    template = replacePlaceholders(template, "title", engineer.getTitle());
+    template = replacePlaceholders(template, "email", engineer.getEmail());
+    template = replacePlaceholders(template, "id", engineer.getID());
+    template = replacePlaceholders(template, "github", engineer.getGithub());
+    return template;
+}
+
+const writeIntern = intern => {
+    let template = fs.readFileSync(path.resolve(cards, "intern.html"), "utf8");
+    template = replacePlaceholders(template, "name", intern.getName());
+    template = replacePlaceholders(template, "title", intern.getTitle());
+    template = replacePlaceholders(template, "email", intern.getEmail());
+    template = replacePlaceholders(template, "id", intern.getID());
+    template = replacePlaceholders(template, "school", intern.getSchool());
+    return template;
 }
