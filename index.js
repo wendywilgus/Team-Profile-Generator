f57a637c700c9d3c565aebe2013183c16ea80b9a
Wendy Wilgus <wilguswendy@gmail.com>
2022-10-16 12:50:20 -0400

    debugging generate

--- index.js
+++ index.js
@@ -5,10 +5,9 @@ const path = require("path");
 const newFile = path.resolve(__dirname, "dist");
 const filePath = path.join(newFile, "index.html");
 
-
 const Employee = require("./lib/employee");
 const Engineer = require("./lib/engineer");
-const Intern = require("./lib/intern.js");
+const Intern = require("./lib/intern");
 const Manager = require("./lib/manager.js");
 
 //link to script for generating HTML page
@@ -16,160 +15,168 @@ const generateHTML = require("./src/generateHTML");
 let employeeArray = [];
 //Questions to begin app
 
-  const employeeBasic = [
-    {
-      type: "input",
-      message: "What is the employee's name?",
-      name: "name",
-      validate: (nameEntry) => {
-        if (nameEntry) {
-          return true;
-        } else {
-          console.log("Please enter a name for this employee.");
-          return false;
-        }
-      },
+const employeeBasic = [
+  {
+    type: "input",
+    message: "What is the employee's name?",
+    name: "name",
+    validate: (nameEntry) => {
+      if (nameEntry) {
+        return true;
+      } else {
+        console.log("Please enter a name for this employee.");
+        return false;
+      }
     },
-    {
-      type: "input",
-      message: "What is the employee's ID?",
-      name: "ID",
-      validate: (idValidation) => {
-        if (!isNaN(idValidation)) {
-          //possibly write as (!isNaN(parseInt(idValidation)))
-          return true;
-        } else {
-          console.log("Please enter a numeric value for the employee's ID.");
-          return false;
-        }
-      },
+  },
+  {
+    type: "input",
+    message: "What is the employee's id?",
+    name: "id",
+    validate: (idValidation) => {
+      if (!isNaN(idValidation)) {
+        //possibly write as (!isNaN(parseInt(idValidation)))
+        return true;
+      } else {
+        console.log("Please enter a numeric value for the employee's id.");
+        return false;
+      }
     },
-    {
-      type: "input",
-      message: "What is the employee's email address?",
-      name: "email",
-    },
-    {
-      type: "list",
-      message: "What is the employee's title?",
-      choices: ["Manager", "Engineer", "Intern"],
-      name: "title",
-    },
-  ];
- 
-    managerPrompt = [
-      {
-        type: "input",
-        message: "What is the manager's office number?",
-        name: "officeNumber",
-        validate: (idValidation) => {
-          if (!isNaN(idValidation)) {
-            //possibly write as (!isNaN(parseInt(idValidation)))
-            return true;
-          } else {
-            console.log("Please enter a numeric value for the manager's office number.");
-            return false;
-          }
-        },
-      },
-    ];
+  },
+  {
+    type: "input",
+    message: "What is the employee's email address?",
+    name: "email",
+  },
+  {
+    type: "list",
+    message: "What is the employee's title?",
+    choices: ["Manager", "Engineer", "Intern"],
+    name: "title",
+  },
+];
 
-    engineerPrompt = [
-      {
-        type: "input",
-        message: "What is the engineer's Github username?",
-        name: "github",
-        validate: (github) => {
-          if (github) {
-            return true;
-          } else {
-            console.log("Github username is required.");
-            return false;
-          }
-        },
-      },
-    ];
+managerPrompt = [
+  {
+    type: "input",
+    message: "What is the manager's office number?",
+    name: "officeNumber",
+    validate: (idValidation) => {
+      if (!isNaN(idValidation)) {
+        //possibly write as (!isNaN(parseInt(idValidation)))
+        return true;
+      } else {
+        console.log(
+          "Please enter a numeric value for the manager's office number."
+        );
+        return false;
+      }
+    },
+  },
+];
 
-    internPrompt = [
-      {
-        type: "input",
-        message: "What school does the intern attend?",
-        name: "school",
-        validate: (school) => {
-          if (school) {
-            return true;
-          } else {
-            console.log("Name of school is required.");
-            return false;
-          }
-        },
-      },
-    ];
+engineerPrompt = [
+  {
+    type: "input",
+    message: "What is the engineer's Github username?",
+    name: "github",
+    validate: (github) => {
+      if (github) {
+        return true;
+      } else {
+        console.log("Github username is required.");
+        return false;
+      }
+    },
+  },
+];
 
-const init = () =>  {
-  console.log("Please follow the prompts to create your team. ")
-  addEmployee()
+internPrompt = [
+  {
+    type: "input",
+    message: "What school does the intern attend?",
+    name: "school",
+    validate: (school) => {
+      if (school) {
+        return true;
+      } else {
+        console.log("Name of school is required.");
+        return false;
+      }
+    },
+  },
+];
+
+const init = () => {
+  console.log("Please follow the prompts to create your team. ");
+  addEmployee();
 };
 
 const addEmployee = async () => {
-  await inquirer.prompt(employeeBasic)
-    .then((response)  =>  {
-      let name  = response.name;
-      let id = response.id;
-      let email = response.email;
-      let title = response.title;
-      let officeNumber;
-      let github;
-      let school;
+  await inquirer.prompt(employeeBasic).then((response) => {
+    let name = response.name;
+    let id = response.id;
+    let email = response.email;
+    let title = response.title;
+    let officeNumber;
+    let github;
+    let school;
 
-      if (title === "Manager") {
-        inquirer.prompt(managerPrompt).then((response)  =>  {
-          officeNumber = response.officeNumber;
-          let employee = new Manager(name, id, email, officeNumber, title);
-          employeeArray.push(employee);
-          newEmployee(employeeArray);
-        });
-      } else if (title === "Engineer") {
-        inquirer.prompt(engineerPrompt).then((response)  =>  {
-          github = response.github;
-          let employee = new Engineer(name, id, email, github, title);
-          employeeArray.push(employee);
-          newEmployee(employeeArray);
-        });
-      } else if (title === "Intern") {
-        inquirer.prompt(internPrompt).then((response)  =>  {
-          school = response.school;
-          let employee = new Intern(name, id, email, school, title);
-          employeeArray.push(employee);
-          newEmployee(employeeArray);
+    if (title === "Manager") {
+      inquirer.prompt(managerPrompt).then((response) => {
+        officeNumber = response.officeNumber;
+        let employee = new Manager(name, id, email, officeNumber, title);
+        employeeArray.push(employee);
+        newEmployee(employeeArray);
+      });
+    } else if (title === "Engineer") {
+      inquirer.prompt(engineerPrompt).then((response) => {
+        github = response.github;
+        let employee = new Engineer(name, id, email, github, title);
+        employeeArray.push(employee);
+        newEmployee(employeeArray);
+      });
+    } else if (title === "Intern") {
+      inquirer.prompt(internPrompt).then((response) => {
+        school = response.school;
+        let employee = new Intern(name, id, email, school, title);
+        employeeArray.push(employee);
+        newEmployee(employeeArray);
       });
     }
   });
 };
 
-  
-
 const newEmployee = async (array) => {
-  await inquirer.prompt(
-    { type: "confirm",//boolean yes or no
+  await inquirer
+    .prompt({
+      type: "confirm", //boolean yes or no
       name: "newEmployee",
       message: "Do you want to add a new employee?",
-
-    }).then(async(response) =>  {
+    })
+    .then(async (response) => {
+      console.log("response", response);
       let createNew = response.newEmployee;
-      if (await createNew === true) {
+      if ((await createNew) === true) {
         addEmployee();
-      }
-      else if (await createNew === false)  {
-        if (!fs.existsSync(newFile))  {
-          fs.mkdirSync(newFile)
+      } else if ((await createNew) === false) {
+        if (!fs.existsSync(newFile)) {
+          fs.mkdirSync(newFile);
         }
-        fs.writeFile(fileName, generateHTML(array), (error)  =>  {
-          if (error) throw error;
-          console.log("Your Team Profile has been created! Please open the index.html file in the 'dist' folder.")
-       });
+        console.log("filePath", filePath);
+        console.log("array", array);
+        console.log("generateHTML(array)", generateHTML(array));
+        fs.writeFile(filePath, generateHTML(array), (error) => {
+          if (error) {
+            console.log("error", error);
+            return;
+          }
+          console.log(
+            "Your Team Profile has been created! Please open the index.html file in the 'dist' folder."
+          );
+        });
       }
-      });
-}
+    });
+};
 
 init();
--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,65 +1,116 @@
 const path = require("path");
 const fs = require("fs");
-const Employee = require("../lib/employee");
-const Manager = require("../lib/manager");
-const Intern = require('/lib/intern.js')
-const cards = path.resolve(__dirname, 'src/cards');
+// const Employee = require("../lib/employee");
+// const Manager = require("../lib/manager");
+// const Intern = require('../lib/intern')
+const cards = path.resolve(__dirname, "src/cards");
 
-const generateHTML = Employee =>  {
-    const writeHTML = [];
+const replacePlaceholders = (template, placeholder, value) => {
+  console.log("replacePlaceholders", {
+    template,
+    placeholder,
+    value,
+  });
+  const pattern = new RegExp("{{ " + placeholder + " }}", "gm"); // HOT TRASH
+  if (typeof template === "string") {
+    const output = template.replace(pattern, value);
+    console.log("output", output);
+    return output;
+  }
+  return "did not find a string";
+};
 
-    writeHTML.push(Employee
-        .filter(Employee => Employee.getTitle() === "Manager")
-        .map(Manager => writeManager(manager))
-    );
-    writeHTML.push(Employee
-        .filter(Employee => Employee.getTitle() === "Engineer")
-        .map(Manager => writeEngineer(engineer))
-    );
-    writeHTML.push(Employee
-        .filter(Employee => Employee.getTitle() === "Intern")
-        .map(Manager => writeIntern(intern))
-    );
-    return newFile(writeHTML.join(""));
+const generateHTML = (Employee) => {
+  const writeHTML = [];
+
+  writeHTML.push(
+    Employee.filter((Employee) => Employee.getTitle() === "Manager").map(
+      (Manager) => writeManager(Manager)
+    )
+  );
+
+  writeHTML.push(
+    Employee.filter((Employee) => Employee.getTitle() === "Engineer").map(
+      (Engineer) => writeEngineer(Engineer)
+    )
+  );
+  writeHTML.push(
+    Employee.filter((Employee) => Employee.getTitle() === "Intern").map(
+      (Intern) => writeIntern(Intern)
+    )
+  );
+  console.log("writeHTML", writeHTML);
+  console.log("writeHTML.join", writeHTML.join(""));
+  const newfileCreated = createNewFile(writeHTML.join(""));
+  console.log("newfileCreated", newfileCreated);
+  return newfileCreated;
+};
+
+const writeManager = (manager) => {
+  console.log("manager", manager);
+  let template = fs.readFileSync(
+    path.resolve(
+      "/Users/wendywilgus/git/wendywilgus/Team Profile Generator/src/cards/manager.html"
+    ),
+    "utf8"
+  );
+  template = replacePlaceholders(template, "name", manager.getName());
+  // template = replacePlaceholders(template, "title", manager.getTitle());
+  template = replacePlaceholders(template, "email", manager.getEmail());
+  template = replacePlaceholders(template, "id", manager.getId());
+  console.log("template", template);
+  template = replacePlaceholders(
+    template,
+    "officeNumber",
+    manager.getOfficeNumber()
+  );
+  return template;
+};
+
+const writeEngineer = (engineer) => {
+  let template = fs.readFileSync(
+    path.resolve(
+      cards,
+      "/Users/wendywilgus/git/wendywilgus/Team Profile Generator/src/cards/engineer.html"
+    ),
+    "utf8"
+  );
+  template = replacePlaceholders(template, "name", engineer.getName());
+  // template = replacePlaceholders(template, "title", engineer.getTitle());
+  template = replacePlaceholders(template, "email", engineer.getEmail());
+  template = replacePlaceholders(template, "id", engineer.getId());
+  template = replacePlaceholders(template, "github", engineer.getGithub());
+  return template;
+};
+
+const writeIntern = (intern) => {
+  let template = fs.readFileSync(
+    path.resolve(
+      cards,
+      "/Users/wendywilgus/git/wendywilgus/Team Profile Generator/src/cards/intern.html"
+    ),
+    "utf8"
+  );
+  template = replacePlaceholders(template, "name", intern.getName());
+  // template = replacePlaceholders(template, "title", intern.getTitle());
+  template = replacePlaceholders(template, "email", intern.getEmail());
+  template = replacePlaceholders(template, "id", intern.getId());
+  template = replacePlaceholders(template, "school", intern.getSchool());
+  return template;
+};
+
+function createNewFile(html) {
+  return fs.readFileSync(
+    path.resolve(
+      cards,
+      "/Users/wendywilgus/git/wendywilgus/Team Profile Generator/src/cards/main.html"
+    ),
+    "utf8"
+  );
 }
 
-const writeManager = manager => {
-    let template = fs.readFileSync(path.resolve(cards, "manager.html"), "utf8");
-    template = replacePlaceholders(template, "name", manager.getName());
-    template = replacePlaceholders(template, "title", manager.getTitle());
-    template = replacePlaceholders(template, "email", manager.getEmail());
-    template = replacePlaceholders(template, "id", manager.getID());
-    template = replacePlaceholders(template, "officeNumber", manager.getOfficeNumber());
-    return template;
-}
-
-const writeEngineer = engineer => {
-    let template = fs.readFileSync(path.resolve(cards, "engineer.html"), "utf8");
-    template = replacePlaceholders(template, "name", engineer.getName());
-    template = replacePlaceholders(template, "title", engineer.getTitle());
-    template = replacePlaceholders(template, "email", engineer.getEmail());
-    template = replacePlaceholders(template, "id", engineer.getID());
-    template = replacePlaceholders(template, "github", engineer.getGithub());
-    return template;
-}
-
-const writeIntern = intern => {
-    let template = fs.readFileSync(path.resolve(cards, "intern.html"), "utf8");
-    template = replacePlaceholders(template, "name", intern.getName());
-    template = replacePlaceholders(template, "title", intern.getTitle());
-    template = replacePlaceholders(template, "email", intern.getEmail());
-    template = replacePlaceholders(template, "id", intern.getID());
-    template = replacePlaceholders(template, "school", intern.getSchool());
-    return template;
-}
-
-const newFile = html => {
-    const template = fs.readFileSync(path.resolve(cards, "main.html"), "utf8");
-    return replacePlaceholders = (template, placeholder, value) => {
-        const pattern = new RegExp("{{ " + placeholder + "}}", "gm");
-        return template.replace(pattern, value);
-
-    }
+const createHybridNewFile = (html) => {
+  return "dumb stuff goes nowhere";
 };
 
 module.exports = generateHTML;
